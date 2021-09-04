ed3cbed2880bb038b29c9445bc394fb40281bf4c
tomashina <tomislav@agmedia.hr>
2021-09-04 14:59:43 +0200

    fix css and filter

--- resources/views/livewire/front/product-category-list.blade.php
+++ resources/views/livewire/front/product-category-list.blade.php
@@ -2,17 +2,26 @@
     <!-- Toolbar-->
     <div class="d-flex justify-content-center justify-content-sm-between align-items-center pt-2 pb-4 pb-sm-5">
         <div class="d-flex flex-wrap">
+
+            <div class="dropdown me-2 d-sm-none"><a class="btn btn-primary dropdown-toggle collapsed" href="#shop-sidebar" data-bs-toggle="collapse" aria-expanded="false"><i class="ci-filter-alt"></i></a></div>
+
+
             <div class="d-flex align-items-center flex-nowrap me-3 me-sm-4 pb-3">
-                <label class="text-light opacity-75 text-nowrap fs-sm me-2 d-none d-sm-block" for="sorting">Sortiraj:</label>
+                <label class="text-light opacity-75 text-nowrap fs-sm me-2 d-none d-sm-block" for="sorting"></label>
                 <select class="form-select" id="sorting-select" wire:ignore>
+                    <option value="" selected>Sortiraj</option>
                     @foreach (config('settings.sorting_list') as $item)
                         <option value="{{ $item['value'] }}" @if(request()->get('sort') == $item['value']) selected @endif>{{ $item['title'] }}</option>
                     @endforeach
                 </select>
-                <span class="fs-sm text-light opacity-75 text-nowrap ms-2 d-none d-md-block">od {{ $products->total() }} artikala</span>
+
             </div>
         </div>
-        <div class="d-flex pb-3"><a class="nav-link-style nav-link-light me-3" href="#"><i class="ci-arrow-left"></i></a><span class="fs-md text-light">{{ $products->currentPage() }} / {{ $products->lastPage() }}</span><a class="nav-link-style nav-link-light ms-3" href="#"><i class="ci-arrow-right"></i></a></div>
+      <!--  <div class="d-flex pb-3"><a class="nav-link-style nav-link-light me-3" href="#"><i class="ci-arrow-left"></i></a><span class="fs-md text-light">{{ $products->currentPage() }} / {{ $products->lastPage() }}</span><a class="nav-link-style nav-link-light ms-3" href="#"><i class="ci-arrow-right"></i></a></div>-->
+
+        <div class="d-flex pb-3">  <span class="fs-sm text-light opacity-75 text-nowrap ms-2 d-none d-sm-block">Ukupno {{ $products->total() }} artikala</span></div>
+
+
     </div>
     <!-- Products grid-->
     <div class="row mx-n2">
