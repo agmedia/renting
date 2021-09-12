a0b7832061d10d9c5fdfdf2914d18c9ccc52d203
tomashina <tomislav@agmedia.hr>
2021-09-12 09:49:53 +0200

    pagination

--- resources/views/vendor/pagination/bootstrap-livewire.blade.php
+++ resources/views/vendor/pagination/bootstrap-livewire.blade.php
@@ -16,7 +16,6 @@
                         @if (is_string($element))
                                 <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
                         @endif
-
                         {{-- Array Of Links --}}
                         @if (is_array($element))
                             @foreach ($element as $page => $url)
