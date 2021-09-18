c1cd90a482ea9eb88f30b6f314fc22d10be1ee51
tomashina <tomislav@agmedia.hr>
2021-09-18 20:29:00 +0200

    dashboard import

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -112,7 +112,13 @@ class DashboardController extends Controller
                 ]);
 
                 if ($product_id) {
-                    $images   = $import->resolveImages(explode('|', $list[$i]['AP']), $name, $product_id);
+
+                    $list[$i]['AP'] = substr($variable, 0, strpos($list[$i]['AP'], "!"));
+
+                    $slike = explode('|', $list[$i]['AP']);
+
+                    $images   = $import->resolveImages($slike), $name, $product_id);
+
                     $categories = $import->resolveCategories(explode('|', $list[$i]['AU']));
 
                     if ($images) {
