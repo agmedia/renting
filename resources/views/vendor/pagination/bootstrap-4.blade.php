30675b48d159f643693cdc302478761aed1cd8f2
tomashina <tomislav@agmedia.hr>
2021-09-19 19:19:54 +0200

    pagination

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -4,41 +4,45 @@
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                    <span class="page-link" aria-hidden="true"><i class="ci-arrow-left me-2"></i> Prethodna</span>
                 </li>
             @else
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"><i class="ci-arrow-left me-2"></i> Prethodna</a>
                 </li>
             @endif
+        </ul>
+
+        <ul class="pagination">
 
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    <li class="page-item disabled d-none d-sm-block"  aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="page-item active d-none d-sm-block" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <li class="page-item d-none d-sm-block"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                         @endif
                     @endforeach
                 @endif
             @endforeach
+            <ul class="pagination">
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">Sljedeća<i class="ci-arrow-right ms-2"></i></a>
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                    <span class="page-link" aria-hidden="true">Sljedeća<i class="ci-arrow-right ms-2"></i></span>
                 </li>
             @endif
         </ul>
--- resources/views/vendor/pagination/bootstrap-livewire.blade.php
+++ resources/views/vendor/pagination/bootstrap-livewire.blade.php
@@ -1,45 +1,51 @@
 @if ($paginator->hasPages())
     <div class="row mt-4">
-        <div class="col-md-6 d-flex justify-content-center justify-content-md-start  mb-sm-3">
-            <nav>
+        <div class="col-md-12 d-flex justify-content-center   mb-sm-3">
+
+            <nav class="d-flex justify-content-between pt-2" aria-label="Page navigation">
                 <ul class="pagination">
                     @if ($paginator->onFirstPage())
                         <li class="page-item disabled me-2" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                            <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                            <span class="page-link" aria-hidden="true"><i class="ci-arrow-left me-2"></i> Prethodna</span>
                         </li>
                     @else
-                        <li class="page-item me-2"><a class="page-link" href="#top" data-scroll="" wire:click="previousPage" aria-label="@lang('pagination.previous')">&lsaquo;</a></li>
+                        <li class="page-item me-2"><a class="page-link" href="#top" data-scroll="" wire:click="previousPage" aria-label="@lang('pagination.previous')"><i class="ci-arrow-left me-2"></i> Prethodna</a></li>
                     @endif
+                </ul>
+
+                <ul class="pagination">
 
                     @foreach ($elements as $element)
                         {{-- "Three Dots" Separator --}}
                         @if (is_string($element))
-                                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                                <li class="page-item disabled d-none d-sm-block" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
                         @endif
                         {{-- Array Of Links --}}
                         @if (is_array($element))
                             @foreach ($element as $page => $url)
                                 @if ($page == $paginator->currentPage())
-                                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                                        <li class="page-item active d-none d-sm-block" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                                 @else
-                                        <li class="page-item"><a class="page-link" href="#top" data-scroll="" wire:click="gotoPage({{ $page }})">{{ $page }}</a></li>
+                                        <li class="page-item d-none d-sm-block"><a class="page-link" href="#top" data-scroll="" wire:click="gotoPage({{ $page }})">{{ $page }}</a></li>
                                 @endif
                             @endforeach
                         @endif
                     @endforeach
+                </ul>
+                <ul class="pagination">
 
                     @if ($paginator->hasMorePages())
-                            <li class="page-item ms-2"><a class="page-link" href="#top" data-scroll="" wire:click="nextPage" aria-label="@lang('pagination.next')">&rsaquo;</a></li>
+                            <li class="page-item ms-2"><a class="page-link" href="#top" data-scroll="" wire:click="nextPage" aria-label="@lang('pagination.next')">Sljedeća<i class="ci-arrow-right ms-2"></i></a></li>
                     @else
                             <li class="page-item disabled ms-2" aria-disabled="true" aria-label="@lang('pagination.next')">
-                                <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                                <span class="page-link" aria-hidden="true">Sljedeća<i class="ci-arrow-right ms-2"></i></span>
                             </li>
                     @endif
                 </ul>
             </nav>
         </div>
 
-        <div class="col-md-6 text-md-end d-flex justify-content-center justify-content-md-end  justify-content-xs-center mt-2">
+        <div class="col-md-12  d-flex justify-content-center  mt-2">
             <p class="text-sm text-gray-700 leading-5">
                 Prikazano
                 <span class="font-weight-bold">{{ $paginator->firstItem() }}</span>
