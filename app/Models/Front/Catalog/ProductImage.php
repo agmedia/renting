4d1f17ba10e87d041ea04bb391b467f721e0503e
Filip Jankoski <filip@agmedia.hr>
2021-09-20 09:01:58 +0200

    Fix on images.

--- app/Models/Front/Catalog/ProductImage.php
+++ app/Models/Front/Catalog/ProductImage.php
@@ -17,4 +17,26 @@ class ProductImage extends Model
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+
+    /**
+     * @param $value
+     *
+     * @return array|string|string[]
+     */
+    public function getImageAttribute($value)
+    {
+        return str_replace('.jpg', '.webp', $value);
+    }
+
+
+    /**
+     * @param $value
+     *
+     * @return array|string|string[]
+     */
+    public function getThumbAttribute($value)
+    {
+        return str_replace('.webp', '-thumb.webp', $this->image);
+    }
+
 }
