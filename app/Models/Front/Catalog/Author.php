461ca31185c2560ae25be39cd94f15dad604d41d
tomashina <tomislav@agmedia.hr>
2021-09-20 11:32:01 +0200

    fix  letters + log

--- app/Models/Front/Catalog/Author.php
+++ app/Models/Front/Catalog/Author.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class Author
--- app/Models/Front/Catalog/Publisher.php
+++ app/Models/Front/Catalog/Publisher.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class Publisher
