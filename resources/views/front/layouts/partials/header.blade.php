6b93dfc7579bb318ebe81fe45e7fc6e3eac03490
tomashina <tomislav@agmedia.hr>
2021-09-22 13:07:59 +0200

    vedute link

--- resources/views/front/layouts/partials/header.blade.php
+++ resources/views/front/layouts/partials/header.blade.php
@@ -27,7 +27,7 @@
                     <li class="nav-item"><a class="nav-link" href="{{ route('catalog.route', ['group' => 'knjige']) }}"><i class="ci-book d-none d-xl-inline-block align-middle mt-n1 me-1"></i>Knjige</a>
                     <li class="nav-item"><a class="nav-link" href="{{ route('catalog.route.author') }}"><i class="ci-user-circle d-none d-xl-inline-block align-middle mt-n1 me-1"></i>Autori</a>
                     <li class="nav-item"><a class="nav-link" href="{{ route('catalog.route.publisher') }}"><i class="ci-bookmark d-none d-xl-inline-block align-middle mt-n1 me-1"></i>Nakladnici</a>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('catalog.route', ['group' => 'vedute-&-zemljovidi']) }}"><i class="ci-map d-none d-xl-inline-block align-middle mt-n1 me-1"></i>Vedute & zemljovidi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('catalog.route', ['group' => 'zemljovidi-i-vedute']) }}"><i class="ci-map d-none d-xl-inline-block align-middle mt-n1 me-1"></i>Vedute & zemljovidi</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('catalog.route.actions') }}"><i class="ci-discount d-none d-xl-inline-block align-middle mt-n1 me-1"></i>Sniženje</a></li>
                     <li class="nav-item d-inloine-block d-lg-none"><a class="nav-link" href="{{ route('catalog.route.page',['page' => 'o-nama']) }}">O nama</a></li>
                     <li class="nav-item d-inloine-block d-lg-none"><a class="nav-link" href="{{ route('faq') }}">Česta pitanja</a></li>
