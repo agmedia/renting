80fa7bbdcbeef3c9d6142d1648e1a1d726588ead
tomashina <tomislav@agmedia.hr>
2021-09-23 10:56:42 +0200

    dropdown

--- resources/views/livewire/front/partials/author-filter.blade.php
+++ resources/views/livewire/front/partials/author-filter.blade.php
@@ -88,10 +88,15 @@
             @if ($selected_publisher)
                 <div class="widget widget-filter mb-4 pb-4 border-bottom">
                     <h3 class="widget-title">Autor</h3>
-                    <div class="input-group input-group-sm mb-2">
+                    <div class="input-group input-group-sm mb-2 autocomplete">
                         <input class="widget-filter-search form-control rounded-end pe-5" type="text" placeholder="Pretraži autora"><i class="ci-search position-absolute top-50 end-0 translate-middle-y fs-sm me-3"></i>
+                        <div id="myInputautocomplete-list" class="autocomplete-items">
+                            <div>
+                                <strong>Dos</strong>tojevski<input type="hidden" value="Albania">
+                            </div>
+                        </div>
                     </div>
-                    <ul class="widget-list widget-filter-list list-unstyled pt-1" style="max-height: 11rem;" data-simplebar data-simplebar-auto-hide="false">
+                 <!--   <ul class="widget-list widget-filter-list list-unstyled pt-1" style="max-height: 11rem;" data-simplebar data-simplebar-auto-hide="false">
                         @foreach ($authors as $author)
                             <li class="widget-filter-item d-flex justify-content-between align-items-center mb-1">
                                 <div class="form-check">
@@ -100,7 +105,7 @@
                                 </div><span class="fs-xs text-muted">{{ $author->broj }}</span>
                             </li>
                         @endforeach
-                    </ul>
+                    </ul>-->
                 </div>
             @endif
 
@@ -109,10 +114,15 @@
             @if ($selected_author)
                 <div class="widget widget-filter mb-4 pb-4 border-bottom">
                     <h3 class="widget-title">Nakladnici</h3>
-                    <div class="input-group input-group-sm mb-2">
+                    <div class="input-group input-group-sm mb-2 autocomplete">
                         <input class="widget-filter-search form-control rounded-end pe-5" type="text" placeholder="Pretraži nakladnika"><i class="ci-search position-absolute top-50 end-0 translate-middle-y fs-sm me-3"></i>
+                        <div id="myInputautocomplete-list" class="autocomplete-items">
+                            <div>
+                                <strong>Lje</strong>vak<input type="hidden" value="Albania">
+                            </div>
+                        </div>
                     </div>
-                    <ul class="widget-list widget-filter-list list-unstyled pt-1" style="max-height: 11rem;" data-simplebar data-simplebar-auto-hide="false">
+                <!--    <ul class="widget-list widget-filter-list list-unstyled pt-1" style="max-height: 11rem;" data-simplebar data-simplebar-auto-hide="false">
                         @foreach ($publishers as $publisher)
                             <li class="widget-filter-item d-flex justify-content-between align-items-center mb-1">
                                 <div class="form-check">
@@ -121,7 +131,7 @@
                                 </div><span class="fs-xs text-muted">{{ $publisher->broj }}</span>
                             </li>
                         @endforeach
-                    </ul>
+                    </ul>-->
                 </div>
             @endif
 
