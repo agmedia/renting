9cd8fb07d497ec1e9ae4edfe6534e3ec164ccce4
tomashina <tomislav@agmedia.hr>
2021-09-24 16:11:36 +0200

    fix iz medija

--- resources/views/front/layouts/widget/widget_product_carousel.blade.php
+++ resources/views/front/layouts/widget/widget_product_carousel.blade.php
@@ -8,7 +8,7 @@
                 <div class="tns-carousel pt-4">
                     <div class="tns-carousel-inner" data-carousel-options='{"items": 2, "controls": true, "nav": true, "autoHeight": true, "responsive": {"0":{"items":2, "gutter": 10},"500":{"items":2, "gutter": 18},"768":{"items":3, "gutter": 20}, "1100":{"items":4, "gutter": 30}}}'>
                     @foreach ($data['items'] as $product)
-
+                        <!-- Product-->
                             <div>
                                 @include('front.catalog.category.product')
                             </div>
@@ -23,7 +23,7 @@
         <div class="tns-carousel pt-4">
             <div class="tns-carousel-inner" data-carousel-options='{"items": 2, "controls": true, "nav": true, "autoHeight": true, "responsive": {"0":{"items":2, "gutter": 10},"500":{"items":2, "gutter": 18},"768":{"items":3, "gutter": 20}, "1100":{"items":4, "gutter": 30}}}'>
             @foreach ($data['items'] as $product)
-
+                <!-- Product-->
                     <div>
                         @include('front.catalog.category.product')
                     </div>
