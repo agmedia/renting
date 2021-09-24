c5d44da4c36a2c3333bb4439e5cbec95f945b9b1
tomashina <tomislav@agmedia.hr>
2021-09-24 14:07:57 +0200

    fix widget

--- resources/views/livewire/back/marketing/action-group-list.blade.php
+++ resources/views/livewire/back/marketing/action-group-list.blade.php
@@ -11,7 +11,7 @@
                         <div class="autocomplete" >
                             <div id="myInputautocomplete-list" class="autocomplete-items">
                                 @foreach($search_results as $item)
-                                    <div wire:click="addItem({{ $item->id }})">{{ isset($item->title) ? $item->title : $item->name }}</div>
+                                    <div wire:click="addItem({{ $item->id }})">{{ isset($item->title) ? $item->title : $item->name }} - {{ isset($item->sku) ? $item->sku : '' }}</div>
                                 @endforeach
                             </div>
                         </div>
@@ -35,7 +35,7 @@
                 @foreach ($list as $item)
                     <tr>
                         <td class="font-size-sm">
-                            {{ isset($item['title']) ? $item['title'] : (isset($item['name']) ? $item['name'] : 'WTF') }}
+                            {{ isset($item['title']) ? $item['title'] : (isset($item['name']) ? $item['name'] : 'WTF') }} - {{ isset($item['sku']) ? $item['sku'] : '' }}
                             <input type="hidden" name="action_list[{{ isset($item['id']) ? $item['id'] : '' }}]" value="{{ isset($item['id']) ? $item['id'] : '' }}">
                         </td>
                         <td class="text-right font-size-sm">
