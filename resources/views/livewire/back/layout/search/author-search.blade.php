8e358c8321e0652044d5f36360315574f3c2db23
tomashina <tomislav@agmedia.hr>
2021-09-29 11:47:14 +0200

    edit product

--- resources/views/livewire/back/layout/search/author-search.blade.php
+++ resources/views/livewire/back/layout/search/author-search.blade.php
@@ -1,19 +1,19 @@
 <div class="mb-0 input-group">
-    <input type="search" wire:model.debounce.300ms="search" class="form-control form-control-lg @error('author_id') is-invalid @enderror" id="author-input" placeholder="Dodaj autora..." autocomplete="off">
+    <input type="search" wire:model.debounce.300ms="search" class="form-control  @error('author_id') is-invalid @enderror" id="author-input" placeholder="Dodaj autora..." autocomplete="off">
     <input type="hidden" wire:model="author_id" name="author_id">
     <span class="input-group-append" data-toggle="modal" data-target="#new-author-modal">
-        <a href="javascript:void(0)" wire:click="viewAddWindow" class="btn btn-secondary btn-search"><i class="fa fa-plus pt-2"></i></a>
+        <a href="javascript:void(0)" wire:click="viewAddWindow" class="btn btn-secondary btn-search py-0"><i class="fa fa-plus pt-2"></i></a>
     </span>
 
     <div class="autocomplete p-3" @if( ! $show_add_window) hidden @endif style="position:absolute; z-index:10; top:38px; background-color: #f6f6f6; border: 1px solid #d7d7d7;">
         <div class="row">
-            <div class="mb-4 col-sm-12 col-md-6">
+            <div class="mb-4 col-sm-12 col-md-12">
                 <label class="form-label required" for="input-title">Ime autora</label>
-                <input type="text" class="form-control form-control-lg @if (session()->has('title')) is-invalid @endif" id="input-title" wire:model.defer="new.title" placeholder="">
+                <input type="text" class="form-control  @if (session()->has('title')) is-invalid @endif" id="input-title" wire:model.defer="new.title" placeholder="">
                 @if (session()->has('title')) <label class="small text-danger">Ime autora je obvezno...</label> @endif
             </div>
 
-            <div class="mb-0 mt-4 col-md-12 text-right">
+            <div class="mb-0 mt-1 col-md-12 text-right">
                 <a href="javascript:void(0)" wire:click="makeNewAuthor" class="btn btn-primary btn-save shadow-sm">
                     <i class="align-middle" data-feather="save">&nbsp;</i> Snimi
                 </a>
@@ -22,11 +22,11 @@
     </div>
 
     @if( ! empty($search_results))
-        <div class="autocomplete pt-3" style="position:unset; background-color: #f6f6f6;">
+        <div class="autocomplete pt-1" style="position:absolute; z-index:10; top:38px; background-color: #f6f6f6; border: 1px solid #d7d7d7;width:100%">
             <div id="myInputautocomplete-list" class="autocomplete-items">
                 @foreach($search_results as $author)
-                    <div style="cursor: pointer;" wire:click="addAuthor('{{ $author->id }}')">
-                        <small class="font-weight-lighter">Ime: <strong>{{ $author->title }}</strong><br>
+                    <div style="cursor: pointer;border-bottom: 1px solid #d7d7d7;padding-bottom: 10px;padding-left: 10px;font-size: 16px" wire:click="addAuthor('{{ $author->id }}')">
+                        <small class="font-weight-lighter">Ime: <strong>{{ $author->title }}</strong></small>
                     </div>
                 @endforeach
             </div>
--- resources/views/livewire/back/layout/search/publisher-search.blade.php
+++ resources/views/livewire/back/layout/search/publisher-search.blade.php
@@ -1,15 +1,15 @@
 <div class="mb-0 input-group">
-    <input type="search" wire:model.debounce.300ms="search" class="form-control form-control-lg @error('publisher_id') is-invalid @enderror" id="publisher-input" placeholder="Dodaj autora..." autocomplete="off">
+    <input type="search" wire:model.debounce.300ms="search" class="form-control  @error('publisher_id') is-invalid @enderror" id="publisher-input" placeholder="Dodaj autora..." autocomplete="off">
     <input type="hidden" wire:model="publisher_id" name="publisher_id">
     <span class="input-group-append" data-toggle="modal" data-target="#new-publisher-modal">
-        <a href="javascript:void(0)" wire:click="viewAddWindow" class="btn btn-secondary btn-search"><i class="fa fa-plus pt-2"></i></a>
+        <a href="javascript:void(0)" wire:click="viewAddWindow" class="btn btn-secondary btn-search py-0"><i class="fa fa-plus pt-2"></i></a>
     </span>
 
     <div class="autocomplete p-3" @if( ! $show_add_window) hidden @endif style="position:absolute; z-index:10; top:38px; background-color: #f6f6f6; border: 1px solid #d7d7d7;">
         <div class="row">
-            <div class="mb-4 col-sm-12 col-md-6">
+            <div class="mb-4 col-sm-12 col-md-12">
                 <label class="form-label required" for="input-title">Ime nakladnika</label>
-                <input type="text" class="form-control form-control-lg @if (session()->has('title')) is-invalid @endif" id="input-title" wire:model.defer="new.title" placeholder="">
+                <input type="text" class="form-control  @if (session()->has('title')) is-invalid @endif" id="input-title" wire:model.defer="new.title" placeholder="">
                 @if (session()->has('title')) <label class="small text-danger">Ime nakladnika je obvezno...</label> @endif
             </div>
 
@@ -22,11 +22,11 @@
     </div>
 
     @if( ! empty($search_results))
-        <div class="autocomplete pt-3" style="position:unset; background-color: #f6f6f6;">
+        <div class="autocomplete pt-1" style="position:absolute; z-index:10; top:38px; background-color: #f6f6f6; border: 1px solid #d7d7d7;width:100%">
             <div id="myInputautocomplete-list" class="autocomplete-items">
                 @foreach($search_results as $publisher)
-                    <div style="cursor: pointer;" wire:click="addPublisher('{{ $publisher->id }}')">
-                        <small class="font-weight-lighter">Ime: <strong>{{ $publisher->title }}</strong><br>
+                    <div style="cursor: pointer;border-bottom: 1px solid #d7d7d7;padding-bottom: 10px;padding-left: 10px;font-size: 16px" wire:click="addPublisher('{{ $publisher->id }}')">
+                        <small class="font-weight-lighter">Ime: <strong>{{ $publisher->title }}</small>
                     </div>
                 @endforeach
             </div>
