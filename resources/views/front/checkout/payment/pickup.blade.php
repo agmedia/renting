32948886c9d906f9419641cf2ff06d3baae72825
tomashina <tomislav@agmedia.hr>
2021-09-30 09:07:14 +0200

    fixex checkout

--- resources/views/front/checkout/payment/pickup.blade.php
+++ resources/views/front/checkout/payment/pickup.blade.php
@@ -5,7 +5,7 @@
         <a class="btn btn-secondary d-block w-100" href="{{ route('naplata') }}"><i class="ci-arrow-left  me-1"></i><span class="d-none d-sm-inline">Povratak na plaćanje</span><span class="d-inline d-sm-none">Povratak</span></a>
     </div>
     <div class="w-50 ps-2">
-        <button class="btn btn-primary d-block w-100" type="submit"><span class="d-none d-sm-inline">Dovršite narudžbu</span><span class="d-inline d-sm-none">Nastavi</span><i class="ci-arrow-right  ms-1"></i></button>
+        <button class="btn btn-primary d-block w-100" type="submit"><span class="d-none d-sm-inline">Dovršite narudžbu</span><span class="d-inline d-sm-none">Dovrši kupnju</span><i class="ci-arrow-right  ms-1"></i></button>
     </div>
     </div>
 </form>
