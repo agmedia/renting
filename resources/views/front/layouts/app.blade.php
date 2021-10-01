93ed2c62fd0a6651c9255b538b1fa5fea261fc32
Filip Jankoski <filip@agmedia.hr>
2021-10-01 10:53:02 +0200

    Fix app loading. livewire only on checkout..

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -14,16 +14,12 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{ config('settings.images_domain') . 'media/img/favicon-32x32.png' }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ config('settings.images_domain') . 'media/img/favicon-32x32.png' }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ config('settings.images_domain') . 'media/img/favicon-16x16.png' }}">
-
-
     <link rel="apple-touch-icon" sizes="180x180" href="{{ config('settings.images_domain') . 'apple-touch-icon.png' }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ config('settings.images_domain') . 'favicon-32x32.png' }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ config('settings.images_domain') . 'favicon-16x16.png' }}">
-    <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="{{ config('settings.images_domain') . 'safari-pinned-tab.svg' }}" color="#314837">
     <meta name="msapplication-TileColor" content="#314837">
     <meta name="theme-color" content="#ffffff">
-
 
     <!-- Vendor Styles including: Font Icons, Plugins, etc.-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -32,20 +28,18 @@
 
     @stack('css_after')
 
-    @livewireStyles
 </head>
 <!-- Body-->
 <body class="handheld-toolbar-enabled">
 <!-- Topbar-->
 <div class="topbar topbar-light bg-light d-none d-md-block">
     <div class="container">
-        <div class="topbar-text text-nowrap ">
+        <div class="topbar-text text-nowrap">
             <a class="topbar-link me-4" href="tel:+38514816574"><i class="ci-phone"></i> +385 1 48 16 574</a>
             <a class="topbar-link me-4" href="https://www.google.com/maps/place/Biblos/@45.810942,15.9794894,17.53z/data=!4m5!3m4!1s0x4765d7aac4f8b023:0xb60bceb791b31ede!8m2!3d45.8106161!4d15.9816921?hl=hr" target="_blank"><i class="ci-location"></i> Palmotićeva 28, Zagreb </a>
-           <a class="topbar-link d-none d-md-inline-block" href="{{ route('kontakt') }}">  <i class="ci-time"></i> PON-PET: 9-20 | SUB: 9-14</a></div>
-
+           <a class="topbar-link d-none d-md-inline-block" href="{{ route('kontakt') }}"><i class="ci-time"></i> PON-PET: 9-20 | SUB: 9-14</a>
+        </div>
         <div class="ms-3 text-nowrap">
-
             <a class="topbar-link d-none d-md-inline-block" href="{{ route('faq') }}">Česta pitanja</a>
             <a class="topbar-link ms-3 ps-3 border-start border-dark d-none d-md-inline-block" href="{{ route('catalog.route.page',['page' => 'o-nama']) }}">O nama</a>
             <a class="topbar-link ms-3 ps-3 border-start border-dark d-none d-md-inline-block" href="{{ route('kontakt') }}">Kontakt</a>
@@ -75,8 +69,6 @@
 <script src="{{ asset('js/smooth-scroll.polyfills.min.js') }}"></script>
 <!-- Main theme script-->
 <script src="{{ asset('js/theme.min.js') }}"></script>
-
-@livewireScripts
 
 <script>
     $(() => {
