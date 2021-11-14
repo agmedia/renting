1eb5afe381e61bd77966aa8735287bb3d3bb8975
tomashina <tomislav@agmedia.hr>
2021-11-14 13:03:35 +0100

    price fix

--- resources/views/front/catalog/category/product.blade.php
+++ resources/views/front/catalog/category/product.blade.php
@@ -1,8 +1,6 @@
 <div class="card product-card-alt">
-
+    <span class="badge rounded-pill bg-primary mt-3 ms-1 badge-shadow">-20%</span>
     <div class="product-thumb">
-
-
         <div class="product-card-actions">
             <a class="btn btn-light btn-icon btn-shadow fs-base mx-2" href="{{ url($product->url) }}"><i class="ci-eye"></i></a>
             <add-to-cart-btn-simple id="{{ $product->id }}"></add-to-cart-btn-simple>
