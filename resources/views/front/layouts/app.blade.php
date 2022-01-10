80cac038b8703949e5172c47e67b73b79ab33b19
tomashina <tomislav@agmedia.hr>
2022-01-10 15:26:17 +0100

    ?v=1.2

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -122,7 +122,7 @@
 <script src="{{ asset('js/tiny-slider.js?v=1.2') }}"></script>
 <script src="{{ asset('js/smooth-scroll.polyfills.min.js?v=1.2') }}"></script>
 <!-- Main theme script-->
-<script src="{{ asset('js/theme.min.js?v=1.2') }}"></script>
+<script src="{{ asset('js/theme.min.js') }}"></script>
 <script src="{{ asset('js/cart.js?v=1.2') }}"></script>
 
 <script>
