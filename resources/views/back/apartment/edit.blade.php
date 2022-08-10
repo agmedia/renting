18061c034eedb4b9348b9919f47739b4cc070c18
Filip Jankoski <filip@agmedia.hr>
2022-08-10 14:34:27 +0200

    #Fix: tabs

--- resources/views/back/apartment/edit.blade.php
+++ resources/views/back/apartment/edit.blade.php
@@ -57,21 +57,32 @@
                                     <div class="row">
                                         <div class="col-md-12 mt-3 mb-3">
 
-                                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                                                <li class="nav-item" role="presentation">
-                                                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                                            <ul class="nav nav-tabs nav-tabs-block" data-toggle="tabs" role="tablist">
+                                                <li class="nav-item">
+                                                    <a class="nav-link active" href="#btabs-static-home">Home</a>
                                                 </li>
-                                                <li class="nav-item" role="presentation">
-                                                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#btabs-static-profile">Profile</a>
                                                 </li>
-                                                <li class="nav-item" role="presentation">
-                                                    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+                                                <li class="nav-item ml-auto">
+                                                    <a class="nav-link" href="#btabs-static-settings">
+                                                        <i class="si si-settings"></i>
+                                                    </a>
                                                 </li>
                                             </ul>
-                                            <div class="tab-content" id="pills-tabContent">
-                                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-                                                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">+</div>
-                                                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">-</div>
+                                            <div class="block-content tab-content">
+                                                <div class="tab-pane active" id="btabs-static-home" role="tabpanel">
+                                                    <h4 class="font-w400">Home Content</h4>
+                                                    <p>...</p>
+                                                </div>
+                                                <div class="tab-pane" id="btabs-static-profile" role="tabpanel">
+                                                    <h4 class="font-w400">Profile Content</h4>
+                                                    <p>...</p>
+                                                </div>
+                                                <div class="tab-pane" id="btabs-static-settings" role="tabpanel">
+                                                    <h4 class="font-w400">Settings Content</h4>
+                                                    <p>...</p>
+                                                </div>
                                             </div>
 
 
