eb6bc69fbab216f6f06291fb8111bdf40fffba1f
Filip Jankoski <filip@agmedia.hr>
2022-09-09 09:14:57 +0200

    #FIX: header image link

--- resources/views/front/layouts/partials/header.blade.php
+++ resources/views/front/layouts/partials/header.blade.php
@@ -128,7 +128,7 @@
                 <div class="row">
                     <div class="col">
                         <nav class="navbar navbar-expand-lg navbar-light secondary-nav hover-primary-nav">
-                            <a class="navbar-brand" href="index.html"><img class="nav-logo" src="assets/images/logo.svg" alt=""></a>
+                            <a class="navbar-brand" href="{{ route('index') }}"><img class="nav-logo" src="{{ asset('assets/images/logo.svg') }}" alt=""></a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
