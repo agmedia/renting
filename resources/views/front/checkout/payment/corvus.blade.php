5bc6bb514db795d7a881e510f7fbabde0bef3426
tomashina <tomislav@agmedia.hr>
2022-10-10 14:04:52 +0200

    corvus hash fix

--- resources/views/front/checkout/payment/corvus.blade.php
+++ resources/views/front/checkout/payment/corvus.blade.php
@@ -31,14 +31,20 @@
 
 <form name="pay" class="w-100" action="{{ $data['action'] }}" method="POST">
 
-    <input type="hidden" name="store_id" value="{{ $data['merchant'] }}">
-    <input id="require_complete"  name="require_complete" value="false" hidden="true"/>
-    <input type="hidden" name="order_number" value="{{ $data['order_id'] }}">
     <input type="hidden" name="amount" value="{{ $data['total'] }}">
-    <input type="hidden" name="hash" value="{{ $data['md5'] }}">
-    <input id="currency" name="currency" value="{{ $data['currency'] }}" type="hidden"/>
     <input id="cart"  name="cart" value="Web shop kupnja - {{ $data['order_id'] }}" type="hidden"/>
+    <input id="currency" name="currency" value="{{ $data['currency'] }}" type="hidden"/>
     <input id="language" name="language" value="{{ $data['lang'] }}" type="hidden"/>
+    <input type="hidden" name="order_number" value="{{ $data['order_id'] }}">
+
+
+    <input id="require_complete"  name="require_complete" value="false" hidden="true"/>
+    <input type="hidden" name="store_id" value="{{ $data['merchant'] }}">
+
+    <input type="hidden" name="signature" value="{{ $data['md5'] }}">
+
+
+
     <input type="hidden" name="cardholder_name" value="{{ $data['firstname'] }}">
     <input type="hidden" name="cardholder_surname" value="{{ $data['lastname'] }}">
     <input type="hidden" name="cardholder_phone" value="{{ $data['telephone'] }}">
