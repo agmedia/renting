cb2bc33a5c9e4d0d74d442cb683890d2f737f0f6
Filip Jankoski <filip@agmedia.hr>
2022-10-23 18:50:02 +0200

    __ Fix set:actions CRON.

--- app/Console/Commands/SetActions.php
+++ app/Console/Commands/SetActions.php
@@ -3,11 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Helpers\ActionHelper;
-use App\Models\Back\Apartment\Apartment;
-use App\Models\Back\Marketing\Action\Action;
 use Illuminate\Console\Command;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Log;
 
 class SetActions extends Command
 {
@@ -42,50 +38,7 @@ class SetActions extends Command
      */
     public function handle()
     {
-        $start = microtime(true);
-
-        $actions = Action::query()->select('id', 'date_start', 'date_end', 'status')
-                         ->where('status', 1)
-                         ->get();
-        // Prvo setati akcije prema datumu, jesu status 1 ili 0.
-        foreach ($actions as $action) {
-            $start = $action->date_start ? Carbon::make($action->date_start) : null;
-            $end = $action->date_end ? Carbon::make($action->date_end) : null;
-
-            if (ActionHelper::isActiveByDates($start, $end)) {
-                $action->update(['status' => 1]);
-            } else {
-                $action->update(['status' => 0]);
-            }
-        }
-
-        // Delete all actions on apartments.
-        Apartment::query()->update(['action_id' => 0]);
-
-        $actions = Action::query()->select('id', 'date_start', 'date_end', 'status')
-                         ->where('status', 1)
-                         ->get();
-        // Prvo setati gdje su svi apartmani odabrani.
-        foreach ($actions as $action) {
-            if ($action->group == 'all') {
-                Apartment::update(['action_id' => $action->id]);
-            }
-        }
-        // Onda setati individualne apartmane.
-        foreach ($actions as $action) {
-            if ($action->group == 'apartment') {
-                Apartment::whereIn('id', json_decode($action->links))->update(
-                    ['action_id' => $action->id]
-                );
-            }
-        }
-
-        sleep(5);
-
-        $end = microtime(true);
-        Log::info('__Set:Actions - Total Execution Time: ' . number_format(($end - $start), 2, ',', '.') . ' sec.');
-
-        return 1;
+        return ActionHelper::runSetupCron();
     }
 
 }
--- app/Helpers/ActionHelper.php
+++ app/Helpers/ActionHelper.php
@@ -2,6 +2,10 @@
 
 namespace App\Helpers;
 
+use App\Models\Back\Apartment\Apartment;
+use App\Models\Back\Marketing\Action\Action;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class ActionHelper
 {
@@ -20,7 +24,7 @@ class ActionHelper
 
         $start = ! $date_start; // Same as ...( ! $date_start) ? true : false;
         $end   = ! $date_end;
-        $now = now();
+        $now   = now();
 
         if ($date_start && $date_start <= $now) {
             $start = true;
@@ -34,7 +38,55 @@ class ActionHelper
             return true;
         }
 
-        return false;   
+        return false;
+    }
+
+
+    /**
+     * @return int
+     */
+    public static function runSetupCron(): int
+    {
+        $log_start = microtime(true);
+
+        $actions = Action::query()->select('id', 'date_start', 'date_end', 'status')->get();
+        // Prvo setati akcije prema datumu, jesu status 1 ili 0.
+        foreach ($actions as $action) {
+            $start = $action->date_start ? Carbon::make($action->date_start) : null;
+            $end   = $action->date_end ? Carbon::make($action->date_end) : null;
+
+            if (ActionHelper::isActiveByDates($start, $end)) {
+                $action->update(['status' => 1]);
+            } else {
+                $action->update(['status' => 0]);
+            }
+        }
+
+        // Delete all actions on apartments.
+        Apartment::query()->update(['action_id' => 0]);
+
+        $actions = Action::query()->select('id', 'group', 'links', 'status')
+                         ->where('status', 1)
+                         ->get();
+        // Prvo setati gdje su svi apartmani odabrani.
+        foreach ($actions as $action) {
+            if ($action->group == 'all') {
+                Apartment::query()->update(['action_id' => $action->id]);
+            }
+        }
+        // Onda setati individualne apartmane.
+        foreach ($actions as $action) {
+            if ($action->group == 'apartment') {
+                Apartment::query()->whereIn('id', json_decode($action->links))->update(
+                    ['action_id' => $action->id]
+                );
+            }
+        }
+
+        $log_end = microtime(true);
+        Log::info('__Set:Actions - Total Execution Time: ' . number_format(($log_end - $log_start), 2, ',', '.') . ' sec.');
+
+        return 1;
     }
 
 }
