6275d7d93ba5badf65c2fb1fea44ff5900246be6
tomashina <tomislav@agmedia.hr>
2023-01-31 09:16:26 +0100

    lang flags header

--- resources/views/front/layouts/partials/language-selector.blade.php
+++ resources/views/front/layouts/partials/language-selector.blade.php
@@ -1,7 +1,7 @@
 
 <div class="language">
     <div class="dropdown hover-dropdown">
-        <button class="dropdown-toggle text-primary" type="button" data-bs-toggle="dropdown">{{ \Illuminate\Support\Str::upper(current_locale()) }}</button>
+        <button class="dropdown-toggle text-primary" type="button" data-bs-toggle="dropdown"><img class="lang" style="width:16px" src="{{ asset('assets/images/'.\Illuminate\Support\Str::upper(current_locale()).'.png') }}" alt=""> {{ \Illuminate\Support\Str::upper(current_locale()) }} </button>
         <ul class="dropdown-menu">
             @if (isset($langs))
                 @foreach ($langs as $lang)
@@ -12,7 +12,7 @@
             @else
                 @foreach (ag_lang() as $lang)
                     <li>
-                        <a class="dropdown-item @if (current_locale() == $lang->code) active @endif" href="{{ LaravelLocalization::getLocalizedURL($lang->code, null, [], true) }}">{{ $lang->title->{LaravelLocalization::getCurrentLocale()} }}</a>
+                        <a class="dropdown-item @if (current_locale() == $lang->code) active @endif" href="{{ LaravelLocalization::getLocalizedURL($lang->code, null, [], true) }}"><img class="lang" style="width:16px" src="{{ asset('assets/images/'.Str::upper($lang->code).'.png') }}" alt=""> {{ $lang->title->{LaravelLocalization::getCurrentLocale()} }}</a>
                     </li>
                 @endforeach
             @endif
