d078cea6aa7a9912548f68084dc1560b7cf3d97c
tomashina <tomislav@agmedia.hr>
2023-02-09 14:08:19 +0100

    footeer text

--- resources/lang/hr/front/success.php
+++ resources/lang/hr/front/success.php
@@ -16,13 +16,13 @@ return [
     'success_main_title' => 'Rezervacija uspješna',
 
     'success_sub_title' => 'Vaša rezervacija je uspješno dovršena!',
-    'success_text_line' => 'Dobit ćete e-mail  s detaljnim podacima o rezervaciji.<br>Naši djelatnici će se uskoro javiti s detaljima i daljnim koracima. <br>Hvala Vam na povjerenju!',
+    'success_text_line' => 'Dobit ćete e-mail  s detaljnim podacima o rezervaciji.<br>Čim uplatite pošaljite nam potvrdu o uplati najbolje na Whatsapp: +385 99 500 8000 <br>Whatsapp LINK: <a href="https://wa.me/message/RJPBBSQGMQKVI1">https://wa.me/message/RJPBBSQGMQKVI1</a><br> Hvala Vam na povjerenju!',
     'success_home' => 'Naslovnica',
     'success_btn' => 'Nastavite pregled stranice',
     'success_text' => 'Uredno smo zaprimili Vašu narudžbu broj',
-    'success_html_text' => '    <p>Molimo vas da izvršite uplatu po sljedećim uputama za plaćanje.</p>
-                                <p> Rok za uplatu je maksimalno 48h.</p>
-                                <p> Ukoliko u tom roku ne zaprimimo uplatu, nažalost moramo poništiti ovu narudžbu.</p>',
+    'success_html_text' => '    <p>Please make the payment according to the following payment instructions.</p>
+                                <p> The deadline for payment is until the apartment is paid for by another guest.</p>
+                                <p> If you are paying later, please check the availability of the apartment before paying.</p>',
     'payment_text' =>'MOLIMO IZVRŠITE UPLATU U IZNOSU OD',
     'refference_number' =>'POZIV NA BROJ',
     'scant_text' =>'ILI JEDNOSTAVNO POSKENIRAJTE 2D BARKOD',
