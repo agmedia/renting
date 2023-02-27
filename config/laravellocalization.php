d22f800c731c79f6281c69d9489b2a4295875f10
Filip Jankoski <filip@agmedia.hr>
2023-02-27 10:55:43 +0100

    _fix_: Images ignored on multi-lang.

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -342,7 +342,7 @@ return [
 
     // URLs which should not be processed, e.g. '/nova', '/nova/*', '/nova-api/*' or specific application URLs
     // Defaults to []
-    'urlsIgnored' => ['/api/*'],
+    'urlsIgnored' => ['/api/*', '/media/*'],
 
     'httpMethodsIgnored' => ['POST', 'PUT', 'PATCH', 'DELETE'],
 ];
